abec9634773d98f618fe571841cdd0e00330a545
PepeDeSoto <pepesoto240@gmail.com>
2021-04-14 14:08:58 +0200

    arreglando links heroku

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/repositoriobluumi/blogigniter/';
+$config['base_url'] = 'https://blogigniter.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
